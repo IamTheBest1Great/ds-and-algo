548af277a61b3131be624dc20045958255796b4f
skhey <109384168+skhey@users.noreply.github.com>
2026-02-08 12:44:25 +0530

    Create Pattern 1 Paranthesis and bracket processing.cpp

---
+++ stack/Pattern 1 Paranthesis and bracket processing.cpp
@@ -0,0 +1,37 @@
+Subtype 1. Basic validation
+
+20. Valid Parentheses
+
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        int n=s.size();
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                if (s[i] == ')' && st.top() != '(') return false;
+                if (s[i] == '}' && st.top() != '{') return false;
+                if (s[i] == ']' && st.top() != '[') return false;
+
+                st.pop();
+            }  
+
+        }
+        return st.empty();
+    }
+};
