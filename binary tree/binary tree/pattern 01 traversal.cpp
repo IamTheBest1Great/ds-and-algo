a6b1a77349e722b378e840274390f366d27a030d
monster <109384168+IamTheBest1Great@users.noreply.github.com>
2026-05-30 00:12:15 +0530

    Update pattern 01 traversal.cpp

--- binary tree/binary tree/pattern 01 traversal.cpp
+++ binary tree/binary tree/pattern 01 traversal.cpp
@@ -66,3 +66,102 @@ public:
         return preorder;
     }
 };
+
+-----------------------------------------------------------------
+101. Symmetric Tree
+    https://leetcode.com/problems/symmetric-tree/description/
+    
+
+class Solution {
+public:
+    bool helper(TreeNode *root1, TreeNode *root2)
+    {
+        if(root1==NULL && root2==NULL)
+        {
+            return true;
+        }
+        if((root1==NULL && root2!=NULL)|| (root1!=NULL && root2==NULL))
+        {
+            return false;
+        }
+        if(root1!=root2)
+        {
+            return false;
+        }
+        return helper(root1->left,root2->right)&&helper(root1->right && root2->left);
+    }
+    bool isSymmetric(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return false;
+        }    
+        return helper(root1,root2);
+    }
+};
+---------------------------------------------------------------
+
+Invert Binary Tree
+    https://leetcode.com/problems/invert-binary-tree/description/
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        TreeNode *temp = new TreeNode;
+        temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
+
+------------------------------------------------------------
+Binary Tree Right Side View
+    
+https://leetcode.com/problems/binary-tree-right-side-view/submissions/1881737947/
+
+    class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        if(root==NULL)
+        {
+            return {};
+        }    
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            
+            for(int i=0;i<n;i++)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                if(i==n-1)
+                {
+                    ans.push_back(temp->val);
+                }
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+---------------------------------------------------------------------------------
+    
